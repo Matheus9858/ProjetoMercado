0f67c1e88fe6749930a91872a69652b26606a633
matheus9858 <matheuspimentelpilotto@id.uff.br>
2022-07-05 17:27:45 -0300

    Update Estoque.java
    implementado as funcoes

--- ProjetoMercado/src/main/java/Main/Estoque.java
+++ ProjetoMercado/src/main/java/Main/Estoque.java
@@ -4,11 +4,53 @@
  */
 package Main;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author pcezar
  */
-public class Estoque {
-    private boolean disponibilidade;
-    
+public class Estoque extends Localizacao {
+
+    private ArrayList<Produto> produtos = new ArrayList<>();
+    private ArrayList<Integer> quantidadesDoProduto=new ArrayList<>();// qtd respectiva  de cada produto no estoque da mesma possição do arrayList de produtos
+
+    public Estoque(String localizacao) {
+        super(localizacao);
+    }
+    public void adicionaNoEstoque(Produto x,int quantidade){
+        produtos.add(x);
+        quantidadesDoProduto.add(quantidade);
+    }
+    public boolean checaDisponibilidade(String x){
+        for(int i=0;i<produtos.size();i++){
+            if(produtos.get(i).getNome().equals(x)){
+                return true;
+            }
+        }
+        
+        return false;
+                
+    }
+    public int checaQuantidade(String x){
+        if (checaDisponibilidade(x)== true){
+        for(int i=0;i<produtos.size();i++){
+            if(produtos.get(i).getNome().equals(x)){
+                return quantidadesDoProduto.get(i);
+                }
+            }
+        }
+    return -1;
+    }
+    public void retiraEstoque(int quantidade,String x){
+        int qtd=checaQuantidade(x);
+        if(qtd<=0){System.out.println("quantidade acima da disponivel");}
+        for(int i=0;i<produtos.size();i++){
+            if(produtos.get(i).getNome().equals(x)){
+                int numero=qtd-quantidade;
+                quantidadesDoProduto.set(i,numero);
+            }
+        }
+                
+    }
 }
